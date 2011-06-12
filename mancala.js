4c360071fa8de74d257085515f1b1cbf2406f8ae
Andrew Jones <me@andrewhjon.es>
2011-06-12 20:04:14 +0100

    neaten code

--- mancala.js
+++ mancala.js
@@ -8,27 +8,47 @@ var game = (function () {
     };
 
     var play = function (board, index) {
-        var b = board.slice(),
-            c = b[index],
+        var b,
+            c = count(board, index),
             i = index,
-            skip;
+            p = player(index);
 
-        if (index < 6) {
-            skip = 13;
-        } else {
-            skip = 6;
-        }
+        var next_index = function (index, player) {
+            var mancala = [6, 13],
+                i = index;
 
-        b[index] = 0;
-        while (c > 0) {
-            if (++i == skip) {
-                ++i;
+            if ((i+1) % 14 == mancala[(player+1) % 2]) {
+                return (i+2) % 14;
             }
+            return (i+1)%14;
+        };
 
-            b[i%14]++;
-            c--;
+        b = zero(board, i);
+        while (c-- > 0) {
+            i = next_index(i, p);
+            b = inc(b, i);
         }
 
+        return b;
+    };
+
+    var player = function (index) {
+        return (index<6) ? 0 : 1;
+    };
+
+    var count = function (board, index) {
+        return board[index%14];
+    };
+
+    var inc = function (board, index) {
+       var b = board.slice();
+       b[index%14]++;
+       return b;
+    };
+
+    var zero = function (board, index) {
+        var b = board.slice();
+        b[index%14] = 0;
         return b;
     };
 
